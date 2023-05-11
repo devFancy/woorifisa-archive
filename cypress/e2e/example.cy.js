05f72df19e9d5e6dfd3f4f8d29aa7dd9d33a9d42
junyong <jymoon1121@gmail.com>
2023-05-11 22:27:43 +0900

    test: 정상 작동 확인을 위한 테스트 코드 추가

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
   it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.visit('/') // localhost:5173에 접속한다.
+    cy.contains('h1', 'todos') // 접속한 페이지 내 렌더링된 엘리먼트들 중에서 h1 태그는 You did it! 이라는 문자열을 포함하고 있다.
   })
 })
