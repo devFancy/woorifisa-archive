ca1cdfd9e86401a32e6cd7e44988ba146563e977
박지윤 <jyun1937@naver.com>
2023-05-18 12:04:05 +0900

    Add: upload project

--- src/main.js
+++ src/main.js
@@ -5,13 +5,11 @@ import { createPinia } from 'pinia';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faLink, faRotateRight, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
-import VueCodeHighlight from 'vue-code-highlight';
 
 const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia());
 app.use(router);
-app.use(VueCodeHighlight);
 
 app.mount('#app');
 library.add(faRotateRight, faThumbsUp, faThumbsDown, faLink);
