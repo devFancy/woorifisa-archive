dc4643b02347eed05a3d445d3bb7be4027fed8d2
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-20 09:39:11 +0900

    Initial commit

---
+++ src/test/java/dev/syntax/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.syntax.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
