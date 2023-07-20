5c38d93b490aca823938807f71da3dd79ae02b81
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-20 09:40:38 +0900

    add: 리드미, 컨트롤러 추가

--- src/main/java/dev/syntax/jenkinsdemo/HelloController.java
+++ src/main/java/dev/syntax/jenkinsdemo/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package dev.syntax.jenkinsdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
